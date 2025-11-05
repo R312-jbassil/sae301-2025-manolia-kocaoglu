b17d03cda18f60b9897e79cea84c963d3da1b764
KOCAOGLU Manolia <157006573+manolia-kocaoglu@users.noreply.github.com>
2025-11-05 13:09:58 +0100

    Integrate Tailwind CSS with Astro configuration and remove legacy Tailwind config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
+
 export default defineConfig({
-    output: 'server'
-});
+    output: 'server',
+    integrations: [tailwind({
+      applyBaseStyles: false, // On gère tout dans global.css
+    })]
+  });
