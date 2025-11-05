eb49114e5ad71d4a1ca442d8abf245cd2e3e04a4
KOCAOGLU Manolia <157006573+manolia-kocaoglu@users.noreply.github.com>
2025-11-05 13:27:42 +0100

    Add Tailwind CSS and DaisyUI configuration, update global styles, and create PostCSS setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
-
 export default defineConfig({
-    output: 'server',
-    integrations: [tailwind({
-      applyBaseStyles: false, // On gère tout dans global.css
-    })]
-  });
+  output: 'server',
+  integrations: [tailwind({ applyBaseStyles: false })]
+});
